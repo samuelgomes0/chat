23386ca9b3d8580b93cc330ccc73b3e459a404fb
Samuel Gomes Rosa <samuel_gomes26@hotmail.com>
2023-05-05 19:38:49 -0300

    🎨 style(style.css): add styles for chat screen and join screen 🎨 style(style.css): add styles for user and message components in chat screen 🎨 style(style.css): add styles for message form and message components in chat screen
    ✨ feat(style.sass): add new colors for online, offline and away users
    ✨ feat(style.sass): add blur effect to join screen
    ✨ feat(style.sass): add styles for join screen
    ✨ feat(style.sass): add styles for chat screen
    ✨ feat(index.html): add search input to users list
    ✨ feat(index.html): add user profile to side menu
    ✨ feat(index.html): add message input and send button to chat box
    ✨ feat(index.html): add script tags to socket.io and index.js
    
    ✨ feat(index.js): add join screen and search user functionality
    ✨ feat(index.js): add message sending functionality
    ✨ feat(index.js): add message receiving functionality
    ✨ feat(index.js): add insert and remove user functionality
    ✨ feat(index.js): add change username functionality
    🔥 chore(main.js): remove BackEnd class
    ✨ feat(server.js): add join, disconnect, message, and change username functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,55 +1,149 @@
 const socket = io();
 
-class FrontEnd {
-  constructor(socket) {
-    this.socket = socket;
-    this.sendMessage();
-    this.listenToUserConnection();
-    this.listenForMessages();
-  }
+const joinScreen = document.querySelector(".join-screen");
+const blurScreen = document.querySelector(".blur");
+const form = document.querySelector(".join-form");
+const usernameInput = document.getElementById("username");
+const searchUser = document.getElementById("search-user");
+const yourName = document.getElementById("your-name");
 
-  sendMessage() {
-    const messagesForm = document.querySelector(".messages__form");
-    const messagesInput = document.querySelector(".messages__form__input");
+// Get username from local storage or ask for it
+if (localStorage.getItem("username")) {
+  yourName.innerText = localStorage.getItem("username");
 
-    messagesForm.addEventListener("submit", (event) => {
-      event.preventDefault();
+  socket.emit("join", localStorage.getItem("username"));
 
-      if (messagesInput.value) {
-        this.socket.emit("chat message", messagesInput.value);
-        messagesInput.value = "";
-      }
-    });
-  }
+  joinScreen.classList.remove("active");
+  blurScreen.classList.remove("active");
+} else {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  listenToUserConnection() {
-    const messages = document.querySelector(".messages");
-    const joinOrLeftMessage = document.createElement("li");
+    const username = usernameInput.value;
 
-    this.socket.on("user joined", ({ userId }) => {
-      joinOrLeftMessage.innerText = `Usuário ${userId} conectado.`;
-      messages.appendChild(joinOrLeftMessage).classList.add("message");
-      messages.scrollTo(0, messages.scrollHeight);
-    });
+    if (username.length >= 3) {
+      socket.emit("join", username);
 
-    this.socket.on("user left", ({ userId }) => {
-      joinOrLeftMessage.innerText = `Usuário ${userId} desconectado.`;
-      messages.appendChild(joinOrLeftMessage).classList.add("message");
-      messages.scrollTo(0, messages.scrollHeight);
-    });
-  }
+      yourName.innerText = username;
 
-  listenForMessages() {
-    const messages = document.querySelector(".messages");
+      localStorage.setItem("username", username);
 
-    this.socket.on("chat message", (message) => {
-      const userMessage = document.createElement("li");
+      socket.emit("join", username);
 
-      userMessage.innerText = message;
-      messages.appendChild(userMessage).classList.add("message");
-      messages.scrollTo(0, messages.scrollHeight);
-    });
-  }
+      joinScreen.classList.remove("active");
+      blurScreen.classList.remove("active");
+    }
+  });
 }
 
-const frontEnd = new FrontEnd(socket);
+// Search for users
+searchUser.addEventListener("keyup", () => {
+  const searchValue = searchUser.value;
+  const users = document.querySelectorAll(".user");
+  let index = 0;
+
+  for (index; index < users.length; index++) {
+    const username = users[index].querySelector("h3").innerText;
+
+    if (username.toLowerCase().startsWith(searchValue.toLowerCase())) {
+      users[index].style.display = "flex";
+    } else {
+      users[index].style.display = "none";
+    }
+  }
+});
+
+// Send message
+const messageForm = document.querySelector(".message-form");
+const messageInput = document.getElementById("message-input");
+
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const message = messageInput.value;
+
+  if (message.length > 0) {
+    socket.emit("message", message);
+
+    messageInput.value = "";
+  }
+});
+
+// Receive message
+socket.on("message", ({ message, time, username }) => {
+  const messages = document.querySelector(".messages");
+  const messageElement = document.createElement("li");
+  const messageInfo = document.createElement("div");
+  const usernameElement = document.createElement("h3");
+  const timeElement = document.createElement("span");
+  const messageText = document.createElement("p");
+
+  messageElement.classList.add("message");
+  messageInfo.classList.add("message-info");
+  messageText.classList.add("message-text");
+
+  messages.appendChild(messageElement);
+  messageElement.appendChild(messageInfo);
+  messageInfo.appendChild(usernameElement);
+  messageInfo.appendChild(timeElement);
+  messageElement.appendChild(messageText);
+
+  usernameElement.innerText = username;
+  timeElement.innerText = time;
+  messageText.innerText = message;
+
+  messages.scrollTo(0, messages.scrollHeight);
+});
+
+// Insert new user in the list
+socket.on("insert user", ({ username }) => {
+  const usersList = document.querySelector(".users");
+  const userElement = document.createElement("li");
+  const usernameElement = document.createElement("h3");
+  const userInfoElement = document.createElement("div");
+  const userStatusElement = document.createElement("span");
+  const userStatusTextElement = document.createElement("p");
+
+  userElement.classList.add("user");
+  userInfoElement.classList.add("user-info");
+  userStatusElement.classList.add("online");
+
+  usersList.appendChild(userElement);
+  userElement.appendChild(usernameElement);
+  userElement.appendChild(userInfoElement);
+  userInfoElement.appendChild(userStatusElement);
+  userInfoElement.appendChild(userStatusTextElement);
+
+  usernameElement.innerText = username;
+  userStatusTextElement.innerText = "Online";
+
+  usersList.scrollTo(0, usersList.scrollHeight);
+});
+
+// Remove user from the list
+socket.on("remove user", ({ username }) => {
+  const usersList = document.querySelector(".users");
+  const users = document.querySelectorAll(".user");
+  let index = 0;
+
+  for (index; index < users.length; index++) {
+    const usernameElement = users[index].querySelector("h3");
+
+    if (usernameElement.innerText === username) {
+      usersList.removeChild(users[index]);
+    }
+  }
+});
+
+// Change username
+yourName.addEventListener("click", () => {
+  const username = prompt("Enter your new username");
+
+  if (username.length >= 3) {
+    socket.emit("change username", username);
+
+    yourName.innerText = username;
+
+    localStorage.setItem("username", username);
+  }
+});
--- src/server.js
+++ src/server.js
@@ -1,22 +1,70 @@
 import express from "express";
 import path from "path";
-
 import { createServer } from "http";
 import { Server } from "socket.io";
-
-import BackEnd from "./main.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 const __dirname = path.resolve();
-
 const server = createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname + "/public/"));
+app.use(express.static(path.join(__dirname, "/public")));
 
-server.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+io.on("connection", (socket) => {
+  let users = [];
+
+  socket.on("join", (username) => {
+    console.log(`${username} joined`);
+
+    socket.username = username;
+
+    users.push(username);
+
+    socket.broadcast.emit("insert user", {
+      username: socket.username,
+      users,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.username} disconnected`);
+
+    users = users.filter((user) => user !== socket.username);
+
+    socket.broadcast.emit("remove user", {
+      username: socket.username,
+      users,
+    });
+  });
+
+  socket.on("message", (message) => {
+    const time = new Date().toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    io.emit("message", {
+      message,
+      time,
+      username: socket.username,
+    });
+  });
+
+  socket.on("change username", (username) => {
+    const oldUsername = socket.username;
+
+    console.log(`${oldUsername} changed username to ${username}`);
+
+    socket.username = username;
+
+    io.emit("warn change username", {
+      oldUsername,
+      username,
+    });
+  });
 });
 
-new BackEnd(io);
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
