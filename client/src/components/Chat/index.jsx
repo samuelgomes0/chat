7fe1958cb755354ca01b7bd188f8d54466907569
Samuel Gomes Rosa <samuel_gomes26@hotmail.com>
2023-06-13 22:03:21 -0300

    🔥 refactor(Chat/index.jsx): remove searchUser function and filteredUsers state 💄 style(Chat/index.jsx): remove search input and adjust user list to fill the space

--- client/src/components/Chat/index.jsx
+++ client/src/components/Chat/index.jsx
@@ -8,7 +8,6 @@ export default function Chat({ socket }) {
   const [isTyping, setIsTyping] = useState(false);
   const [username, setUsername] = useState("");
   const [lastMessage, setLastMessage] = useState("");
-  const [filteredUsers, setFilteredUsers] = useState([]);
 
   const messageRef = useRef();
   const userTypingRef = useRef();
@@ -45,21 +44,6 @@ export default function Chat({ socket }) {
     socket.emit("change username", newUsername);
 
     setUsername(newUsername);
-  };
-
-  const searchUser = (event) => {
-    const text = event.target.value;
-
-    if (!text.trim()) {
-      setFilteredUsers([]);
-      return;
-    }
-
-    const filtered = onlineUsers.filter((user) =>
-      user.username.toLowerCase().includes(text.toLowerCase())
-    );
-
-    setFilteredUsers(filtered);
   };
 
   const handleTyping = () => {
@@ -129,28 +113,19 @@ export default function Chat({ socket }) {
   return (
     <section className="flex h-screen w-screen gap-4 p-8">
       <div className="flex h-full w-1/4 flex-col justify-between gap-4">
-        <input
-          type="text"
-          placeholder="Search"
-          autoComplete="off"
-          className="w-full rounded-xl border border-gray-300 bg-white p-3.5 text-sm shadow-sm transition-colors hover:bg-neutral-100 focus:border-transparent focus:bg-neutral-100 focus:outline-none focus:ring-2 focus:ring-violet-500"
-          onChange={searchUser}
-        />
         <ul className="h-full overflow-y-auto rounded-xl border border-gray-300 bg-white shadow-sm">
-          {filteredUsers
-            ? filteredUsers
-            : onlineUsers.map(
-                (user, index) =>
-                  user.id !== socket.id && (
-                    <li
-                      key={index}
-                      className="h-20 border-b border-gray-200 p-4 transition-colors hover:cursor-pointer hover:bg-neutral-200"
-                    >
-                      <h3 className="font-semibold">{user.username}</h3>
-                      <p className="text-sm text-gray-500">{lastMessage}</p>
-                    </li>
-                  )
-              )}
+          {onlineUsers.map(
+            (user, index) =>
+              user.id !== socket.id && (
+                <li
+                  key={index}
+                  className="h-20 border-b border-gray-200 p-4 transition-colors hover:cursor-pointer hover:bg-neutral-200"
+                >
+                  <h3 className="font-semibold">{user.username}</h3>
+                  <p className="text-sm text-gray-500">{lastMessage}</p>
+                </li>
+              )
+          )}
         </ul>
         <div className="flex justify-between rounded-xl border border-gray-300 bg-white p-4 text-center shadow-sm">
           <h3
